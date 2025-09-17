fe673ab432e1ea03c08177aa16d45b2f09b227fc
M. Zasada <mzasada93@gmail.com>
2025-09-17 19:46:56 +0200

    chore: init c++

---
+++ Source/h_s/HSGameModeBase.h
@@ -0,0 +1,13 @@
+// All rights reserved. No permission is granted to use, copy, modify, or distribute this software for any purpose without explicit written permission from the author.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HSGameModeBase.generated.h"
+
+UCLASS()
+class H_S_API AHSGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+};
